0474f25453553f677c5cd15e116cf9fbd265e8d7
Beejay <beejay.urzo@gmail.com>
2022-05-29 03:26:30 +0800

    Initial commit for Vonage Audio Merge Sample

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,16 @@
 
 #include "config.h"
 #include "otk_thread.h"
+#include <stdio.h>
+#include <string.h>
 
 static std::atomic<bool> g_is_connected(false);
 
-FILE *audio_out;
+//Define our floor and translator audio files
+FILE *floor_audio_out;
+FILE *translator_audio_out;
 
+bool translator_has_audio = true;
 struct audio_device {
   otc_audio_device_callbacks audio_device_callbacks;
   otk_thread_t renderer_thread;
@@ -26,11 +31,8 @@ static otk_thread_func_return_type renderer_thread_start_function(void *arg) {
 
   while (device->renderer_thread_exit.load() == false) {
   	int16_t samples[160];
-	size_t actual = otc_audio_device_read_render_data(samples,160);
-	if(actual > 0){
-		fwrite(samples, sizeof(int16_t),actual,audio_out);
-	}
-	usleep(10*1000);
+    size_t actual = otc_audio_device_read_render_data(samples,160);
+	  usleep(10*1000);
   }
 
   otk_thread_func_return_value;
@@ -98,6 +100,37 @@ static void on_subscriber_error(otc_subscriber* subscriber,
   std::cout << "Subscriber error. Error code: " << error_string << std::endl;
 }
 
+static void on_subscriber_audio_data(otc_subscriber* subscriber,
+                        void* user_data,
+                        const struct otc_audio_data* audio_data){
+  otc_stream *stream = otc_subscriber_get_stream(subscriber);
+  
+  if ( strcmp(otc_session_get_id(otc_subscriber_get_session(subscriber)), SESSION_ID) ==0 ){
+
+    fwrite(audio_data->sample_buffer, sizeof(audio_data->sample_rate),audio_data->number_of_samples,floor_audio_out);
+
+    //Translator is muted, let's write floor to audio 2
+    if(!translator_has_audio){
+      fwrite(audio_data->sample_buffer, sizeof(audio_data->sample_rate),audio_data->number_of_samples,translator_audio_out);
+      std::cout << "Muted Translator Audio, Writing Floor Audio to Translator File " << std::endl;
+    }
+  
+  } 
+  if ( strcmp(otc_session_get_id(otc_subscriber_get_session(subscriber)), SESSION_ID_2) ==0 ){
+     if (otc_stream_has_audio(stream)){
+      //The stream is not muted, let's mark our translator audio state to true
+      translator_has_audio = true;
+      std::cout << "Active Translator Audio Writing, to Translator File " << std::endl;
+      fwrite(audio_data->sample_buffer, sizeof(audio_data->sample_rate),audio_data->number_of_samples,translator_audio_out);
+    }else{
+      //let's mark our translator audio state to false
+      translator_has_audio = false;
+    }
+
+  } 
+
+}
+
 static void on_session_connected(otc_session *session, void *user_data) {
   std::cout << __FUNCTION__ << " callback function" << std::endl;
 
@@ -126,6 +159,7 @@ static void on_session_stream_received(otc_session *session,
   subscriber_callbacks.on_connected = on_subscriber_connected;
   subscriber_callbacks.on_render_frame = on_subscriber_render_frame;
   subscriber_callbacks.on_error = on_subscriber_error;
+  subscriber_callbacks.on_audio_data = on_subscriber_audio_data;
 
   otc_subscriber *subscriber = otc_subscriber_new(stream,&subscriber_callbacks);
   otc_subscriber_set_subscribe_to_video(subscriber,0);
@@ -178,10 +212,43 @@ int main(int argc, char** argv) {
   otc_log_enable(OTC_LOG_LEVEL_ALL);
 #endif
 
-  if((audio_out = fopen("audio_out.pcm","wb"))==NULL){
+
+  if((floor_audio_out = fopen("floor_audio.pcm","wb"))==NULL){
 	printf("Error opening audio output file\n");
 	exit(1);
-  } 
+  }
+  if((translator_audio_out = fopen("translator_audio.pcm","wb"))==NULL){
+	printf("Error opening audio output file\n");
+	exit(1);
+  }
+
+  //Explore later to write WAV so SR BD and CH will be set automatically
+  // short NumChannels = 2;
+  // short BitsPerSample = 16;
+  // int SamplingRate = 4800;
+
+  // int ByteRate = NumChannels*BitsPerSample*SamplingRate/8;
+  // short BlockAlign = NumChannels*BitsPerSample/8;
+  // int DataSize = NumChannels*1*BitsPerSample/8;
+  // int chunkSize = 16;
+  // int totalSize = 46 + DataSize;
+  // short audioFormat = 1;
+
+  // fwrite("RIFF", sizeof(char), 4,audio_out);
+  // fwrite(&totalSize, sizeof(int), 1, audio_out);
+  // fwrite("WAVE", sizeof(char), 4, audio_out);
+  // fwrite("fmt ", sizeof(char), 4, audio_out);
+  // fwrite(&chunkSize, sizeof(int),1,audio_out);
+  // fwrite(&audioFormat, sizeof(short), 1, audio_out);
+  // fwrite(&NumChannels, sizeof(short),1,audio_out);
+  // fwrite(&SamplingRate, sizeof(int), 1, audio_out);
+  // fwrite(&ByteRate, sizeof(int), 1, audio_out);
+  // fwrite(&BlockAlign, sizeof(short), 1, audio_out);
+  // fwrite(&BitsPerSample, sizeof(short), 1, audio_out);
+  // fwrite("data", sizeof(char), 4, audio_out);
+  // fwrite(&DataSize, sizeof(int), 1, audio_out);
+
+
   struct audio_device *device = (struct audio_device *)malloc(sizeof(struct audio_device));
   device->audio_device_callbacks = {0};
   device->audio_device_callbacks.user_data = static_cast<void *>(device);
@@ -198,6 +265,7 @@ int main(int argc, char** argv) {
   session_callbacks.on_stream_dropped = on_session_stream_dropped;
   session_callbacks.on_disconnected = on_session_disconnected;
   session_callbacks.on_error = on_session_error;
+  
 
   otc_session *session = nullptr;
   session = otc_session_new(API_KEY, SESSION_ID, &session_callbacks);
@@ -208,6 +276,16 @@ int main(int argc, char** argv) {
   }
 
   otc_session_connect(session, TOKEN);
+
+  otc_session *session2 = nullptr;
+  session2 = otc_session_new(API_KEY_2, SESSION_ID_2, &session_callbacks);
+
+  if (session2 == nullptr) {
+    std::cout << "Could not create OpenTok session successfully" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  otc_session_connect(session2, TOKEN_2);
 
   while(1){
 	  sleep(1);
